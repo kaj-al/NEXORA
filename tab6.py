60c50ccf7386b06294647fb49b5aa7d5848ded83
kaj-al <kt2519033@gmail.com>
2026-03-05 22:11:58 +0530

    create vectorstore

--- tab6.py
+++ tab6.py
@@ -1,7 +1,12 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
 import os
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.vectors import FAISS # type: ignore
 
 
 def get_embeddings():
     return HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+
 
